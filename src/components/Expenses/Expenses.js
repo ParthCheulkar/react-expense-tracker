6270718411521d18efa01a2bdf9f580fa36d92d5
Parth Cheulkar <pscheulkar12@gmail.com>
2022-01-12 23:27:00 +0530

    add lists and conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,16 +7,29 @@ export default function Expenses(props) {
   const [yearSelected, setYearSelected] = useState("2020");
   const items = props.data;
   const saveYearHandler = (selectedYear) => {
-    console.log("m in expense js");
-    console.log(selectedYear);
+    // console.log("m in expense js");
+    // console.log(selectedYear);
     setYearSelected(selectedYear);
   }
+  const fiteredList = items.filter((ele) => ele.date.getFullYear().toString() === yearSelected);
+
+  let emptyDataContent = <p>No data found :((</p> ; //u can store JSX content in variables
+
+  //ALTERNATIVE METHOD
+  // if(fiteredList.length > 0){
+  //   emptyDataContent = fiteredList.map((ele) =>
+  //   <ExpenseItem key={ele.id} title={ele.title} amount={ele.amount} date={ele.date} />);
+  // }
     return (
       <Card className="expenses">
         <ExpensesFilter selected = {yearSelected} onSelectYear = {saveYearHandler}/>
-       { items.map((ele) =>
+       {fiteredList.length === 0 &&  emptyDataContent}  
+       {fiteredList.length > 0 && fiteredList.map((ele) =>
         <ExpenseItem key={ele.id} title={ele.title} amount={ele.amount} date={ele.date} />)}
     
       </Card>
     );
 }
+
+//&& is a JS trick wherein if the first part is true, it returns the second part of the expression.
+
