fbfeb167784a2a72a243376eef7aabb7a6f60158
Parth Cheulkar <pscheulkar12@gmail.com>
2022-01-23 12:11:02 +0530

    complete project

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -4,7 +4,6 @@ import "./Chartbar.css";
 
 const ChartBar = (props) => {
   let barFillHeight = "0%";
-
   if (props.maxValue > 0) {
     barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
   }
